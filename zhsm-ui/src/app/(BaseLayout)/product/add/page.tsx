d4cc83c1943ead22f78739817acf69256a4be190
clover <2621869236@qq.com>
2023-08-07 14:11:52 +0800

    fix 在客户端使用服务端组件

--- zhsm-ui/src/app/(BaseLayout)/product/add/page.tsx
+++ zhsm-ui/src/app/(BaseLayout)/product/add/page.tsx
@@ -7,65 +7,19 @@
  * @author: Clover
  * @create: 2023-07-19 16:54
  */
-'use client'
-import MainContent from '#/components/MainContent'
 import type { FC } from 'react'
 
-import { BaseInfoForm } from './components/form/BaseInfoForm'
-import LayoutSpace from '#/components/LayoutSpace'
-import { PriceForm } from './components/form/PriceForm'
-import { InventoryForm } from './components/form/InventoryForm'
-import { InventoryAlertForm } from './components/form/InventoryAlertForm'
-import { FooterAction } from './components/FooterAction'
-import { Form } from 'antd'
 import Auth from '#/components/Security/Login'
+import { ProductAddMain } from './Main'
 
 const ProductAddPage: FC = Auth(
   {
     login: true,
   },
   () => {
-    const [form] = Form.useForm()
-
-    const saveAndAdd = () => {
-      console.log(form.getFieldsValue())
-    }
-
     return (
       <>
-        <MainContent title={'基本信息'}>
-          <BaseInfoForm form={form} />
-        </MainContent>
-
-        <LayoutSpace direction={'vertical'} />
-
-        <MainContent title={'价格管理'}>
-          <PriceForm form={form} />
-        </MainContent>
-
-        <LayoutSpace direction={'vertical'} />
-
-        <MainContent title={'期初库存'}>
-          <InventoryForm />
-        </MainContent>
-
-        <LayoutSpace direction={'vertical'} />
-
-        <MainContent title={'库存预警'}>
-          <InventoryAlertForm />
-        </MainContent>
-
-        <LayoutSpace direction={'vertical'} />
-
-        <MainContent
-          style={{
-            position: 'sticky',
-            bottom: 0,
-          }}
-          size={'small'}
-        >
-          <FooterAction saveAndAdd={saveAndAdd} />
-        </MainContent>
+        <ProductAddMain />
       </>
     )
   },
